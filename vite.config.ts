c4c2b358e16fb9ef60dc66ea75d539cb01873d8f
simranvir sondhi <simranvirsondhi2708@gmail.com>
2026-01-20 18:13:54 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base:"/Simranvirsinghdataanalystportfoliowebsite/",
+    base:"./Simranvirsinghdataanalystportfoliowebsite/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
